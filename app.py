da4a08ed24e807a5162a8f64156fd4042dee2e30
Ольга Бархатова <olya.barkhatova@mail.ru>
2024-12-20 12:28:54 +0700

    правки 2.1

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def main():
     try:
         conn, cur = db_connect()
         if 'user_id' in session:
-            ("""
+            cur.execute("""
                 SELECT ads.id, ads.title, ads.content, users.fullname AS author, users.email
                 FROM ads
                 JOIN users ON ads.user_id = users.id;
@@ -83,7 +83,6 @@ def register():
         if os.path.isdir(avatar_path):
             raise ValueError(f"Invalid file path: {avatar_path}")
         avatar.save(avatar_path)
-
 
         conn, cur = db_connect()
 
